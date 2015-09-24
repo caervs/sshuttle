5a324dff52e5755a803aa3d4e273faac47e6e338
Ryan Abrams <rdabrams@gmail.com>
2015-09-24 15:59:26 -0700

    [sshuttle] python package for sshuttle

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='sshuttle',
+      version='1.0',
+      description='where transparent proxy meets VPN meets ssh',
+      author='apenwarr',
+      author_email='apenwarr@gmail.com',
+      url='https://github.com/apenwarr/sshuttle',
+      packages=['sshuttle'],
+)
